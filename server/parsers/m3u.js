d936caf5e7fcd12bc1b787d39ecc31648beaa751
José Luis Campaña Pérez <macpanda@gmail.com>
2019-03-02 23:16:26 +0100

    M3U file generator

--- server/parsers/m3u.js
+++ server/parsers/m3u.js
@@ -1,10 +1,20 @@
-const { Parser } = require('./parser');
-const fs = require('fs');
+const { Parser } = require("./parser");
+const fs = require("fs");
 
-class ParserM3U extends Parser{
-  constructor(url, {logger, whitelist, countryGroup, removeFromTitle, removeChannelContains} = {}) {
+class ParserM3U extends Parser {
+  constructor(
+    url,
+    {
+      logger,
+      whitelist,
+      countryGroup,
+      removeFromTitle,
+      removeChannelContains,
+      favoriteListById
+    } = {}
+  ) {
     super({ logger });
-    this.filterGroups = []
+    this.filterGroups = [];
     this.channels = [];
     this.groups = {};
     this.channelByName = {};
@@ -14,78 +24,140 @@ class ParserM3U extends Parser{
     this.countryGroup = countryGroup || {};
     this.removeFromTitle = removeFromTitle || [];
     this.removeChannelContains = removeChannelContains || [];
+    this.favoriteListById = favoriteListById || [];
+  }
+
+  get channelList() {
+    return this.channels;
   }
 
   async remote() {
-    const options = {method: 'GET', url: this.url }
+    const options = { method: "GET", url: this.url };
 
-    return super.remote(options).then(res => this.parse(res)).catch(_ => []);
+    return super
+      .remote(options)
+      .then(res => this._parse(res))
+      .catch(_ => []);
   }
 
   async readFromFile(filename) {
     return new Promise((resolve, reject) => {
-      fs.readFile(filename, { encoding: 'utf8' }, (err, data) => {
-        return err ? reject(err) : resolve(data);
-      })
-    })
+      fs.readFile(filename, { encoding: "utf8" }, (err, data) => {
+        return err ? reject(err) : resolve(this._parse(data));
+      });
+    });
+  }
+
+  /**
+   * @method _parse
+   * @private
+   * @param {array} res
+   */
+  async _parse(res) {
+    let channels = res.split("#EXTINF:-1 ");
+
+    return new Promise((resolve, reject) => {
+      if (channels.length && channels.length > 1) {
+        channels.shift();
+        channels = this._map(channels);
+        channels = this._reduceByGroup(channels);
+
+        //Filter by favorites as well
+
+        this._groupChannels(channels);
+
+        resolve(this.channels);
+      } else {
+        reject(new Error("Wrong m3u format"));
+      }
+    });
+  }
+
+  /**
+   * @method export
+   * @private
+   * @param {array} channels
+   */
+  export({ filter } = {}) {
+    const M3U_HEADER = "#EXTM3U\n";
+    let result = "";
+    const filteredChannels = this.channels;
+
+    filteredChannels.forEach(channel => {
+      result = `${result}\n#EXTINF:-1 tvg-ID="${channel.tvgId}" tvg-name="${
+        channel.name2
+      }" tvg-logo="${channel.icon}" group-title="${channel.group}",${
+        channel.name2
+      } (${channel.id})\n${channel.urlIPTV}`;
+    });
+
+    return `${M3U_HEADER}\n${result}`;
   }
 
   _map(channels) {
     const result = channels.map(line => {
-
       try {
-        let data = line.split('\n');
+        let data = line.split("\n");
         const urlIPTV = data[1];
-        const url = data[1].split('/');
+        const url = data[1].split("/");
         const id = url[url.length - 1].trim().slice();
 
-        data = data[0].split('"').join('');
-        data = data.split('group-title=')
-        const group = data[1].split(',')[0].trim().slice();
+        data = data[0].split('"').join("");
+        data = data.split("group-title=");
+        const group = data[1]
+          .split(",")[0]
+          .trim()
+          .slice();
         const country = this.countryGroup[group];
-        data = data[0].split('tvg-logo=');
+        data = data[0].split("tvg-logo=");
 
         const icon = data[1].trim().slice();
-        data = data[0].split('tvg-name=');
+        data = data[0].split("tvg-name=");
 
         const name = data[1].trim().slice();
-        data = data[0].split('tvg-ID=');
+        data = data[0].split("tvg-ID=");
 
         let tvgId = data[1].trim().slice();
 
         let name2 = name.toUpperCase();
         this.removeFromTitle.forEach(item => {
-          name2 = name2.replace(item, '');
+          name2 = name2.replace(item, "");
         });
         name2 = name2.trim();
 
-        if(tvgId === ""){
-          tvgId = name2
+        if (tvgId === "") {
+          tvgId = name2;
         }
-  
-        tvgId=tvgId.replace(/ /g, '_')
+
+        tvgId = tvgId.replace(/ /g, "_");
 
         return { name2, name, country, id, urlIPTV, group, icon, tvgId };
       } catch (err) {
-        this.logger.error(`Unexpected line format: ${line} - error: ${JSON.stringify(err)}`);
+        this.logger.error(
+          `Unexpected line format: ${line} - error: ${JSON.stringify(err)}`
+        );
       }
     });
 
-    return result
+    return result;
   }
 
   _reduceByGroup(channels) {
-    channels = channels.filter(ch => !ch.id.endsWith('.mkv') && !ch.id.endsWith('.avi'))
+    channels = channels.filter(
+      ch => !ch.id.endsWith(".mkv") && !ch.id.endsWith(".avi")
+    );
 
     if (this.whitelist.length) {
       channels = channels.filter(ch => this.whitelist.includes(ch.group));
     }
 
     if (this.removeChannelContains.length) {
-      channels = channels.filter(ch => this.removeChannelContains.reduce((acc, name) => {
-        acc = acc && !ch.name2.includes(name);
-        return acc
-      }, true));
+      channels = channels.filter(ch =>
+        this.removeChannelContains.reduce((acc, name) => {
+          acc = acc && !ch.name2.includes(name);
+          return acc;
+        }, true)
+      );
     }
 
     return channels;
@@ -93,20 +165,18 @@ class ParserM3U extends Parser{
 
   _groupChannels(channels) {
     const groups = [];
-    const names = {}
+    const names = {};
     const groupedChannels = {};
 
     channels.forEach(channel => {
-      
-
       //Group by group
       if (!groups.includes(channel.group)) {
         groups.push(channel.group);
-        groupedChannels[channel.group] = {}
+        groupedChannels[channel.group] = {};
       }
 
-      if(!groupedChannels[channel.group][channel.name2]){
-        groupedChannels[channel.group][channel.name2] = []
+      if (!groupedChannels[channel.group][channel.name2]) {
+        groupedChannels[channel.group][channel.name2] = [];
       }
 
       groupedChannels[channel.group][channel.name2].push(channel);
@@ -122,24 +192,6 @@ class ParserM3U extends Parser{
     this.groups = groups;
     this.channelByName = { ...names };
     this.groupedChannels = { ...groupedChannels };
-  }
-
-  async parse(res) {
-    let channels = res.split('#EXTINF:-1 ');
-
-    return new Promise((resolve, reject) => {
-      if (channels.length && channels.length > 1) {
-        channels.shift();
-        channels = this._map(channels);
-        channels = this._reduceByGroup(channels);
-
-        this._groupChannels(channels);
-
-        resolve(this.channels);
-      } else {
-        reject(new Error('Wrong m3u format'));
-      }
-    });
   }
 }
 
--- test.js
+++ test.js
@@ -1,26 +1,40 @@
-const { ParserM3U } = require('./server/parsers/m3u');
-const {M3UGenerator} = require('./server/generators/m3u');
-const { ParserES} = require('./server/parsers/es');
-const { config } = require('dotenv');
-const { whitelist, countryGroup, removeFromTitle, removeChannelContains } = require('./config.json');
+const { ParserM3U } = require("./server/parsers/m3u");
+const { M3UGenerator } = require("./server/generators/m3u");
+const { ParserES } = require("./server/parsers/es");
+const { config } = require("dotenv");
+const {
+  whitelist,
+  countryGroup,
+  removeFromTitle,
+  removeChannelContains
+} = require("./config.json");
 config(); //Injecting variables to process.env from .env file
-require('./server/validateConfig.js').checkEnv();
+require("./server/validateConfig.js").checkEnv();
 
 const test = async () => {
-  const iptv = new ParserM3U(process.env.IPTV_SERVER, {whitelist,countryGroup,removeFromTitle, removeChannelContains});
-  // await iptv.remote();
+  const iptv = new ParserM3U(process.env.IPTV_SERVER, {
+    whitelist,
+    countryGroup,
+    removeFromTitle,
+    removeChannelContains
+  });
+  // const channels = await iptv.remote();
 
-  const res = await iptv.readFromFile('./channels.m3u');
-  await iptv.parse(res);
+  try {
+    const channels = await iptv.readFromFile("./channels.m3u");
+    console.log(iptv.export());
+  } catch (err) {
+    console.error(err);
+  }
 
-  const generator = new M3UGenerator(iptv.groupedChannels,{groups: ["ESPAÑA"]})
-  const m3u = await generator.generate();
+  // const generator = new M3UGenerator(iptv.groupedChannels,{groups: ["ESPAÑA"]})
+  // const m3u = await generator.generate();
 
-  const es = new ParserES({numDays: 7});
-  const epg = await es.remote();
+  // const es = new ParserES({numDays: 7});
+  // const epg = await es.remote();
 
-  console.log(m3u);
-  console.log(epg);
-}
+  // console.log(m3u);
+  // console.log(epg);
+};
 
 test();
